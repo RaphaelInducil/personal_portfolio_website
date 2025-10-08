afc48d0391d1c9d63b0f0f37dd879ac69608977a
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-10-08 21:37:30 +0800

    check point muna, naayos ko na yung mga di gumaganang links

--- Flask_Intro/app.py
+++ Flask_Intro/app.py
@@ -6,18 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/intro')
-def intro():
-    return render_template('intro.html')
-
-@app.route('/home')
-def home():
-    return render_template('homepage.html')
-
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
-
 @app.route('/works')
 def works():
     return render_template('works.html')
@@ -29,10 +17,6 @@ def touppercase():
         input_string = request.form.get('inputString', '')
         result = input_string.upper()
     return render_template('touppercase.html', result=result)
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
 
 @app.route('/works/area/circle', methods=['GET', 'POST'])
 def acircle():
@@ -50,6 +34,18 @@ def atriangle():
         height = request.form.get('height', '')
         result = (int(width)*int(height))*.5
     return render_template('triangle.html', result=result)
+
+@app.route('/home')
+def home():
+    return render_template('homepage.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
