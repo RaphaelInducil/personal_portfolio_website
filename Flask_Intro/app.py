c7bd668d7d8ee65a8f5f2ead94443ce4abdaa117
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-10-29 19:14:50 +0800

    infix-postfix coding, change name of infix-postifx to stack_algo, routing pathways from app-work-stack_algo.

--- Flask_Intro/app.py
+++ Flask_Intro/app.py
@@ -35,6 +35,39 @@ def atriangle():
         result = (int(width)*int(height))*.5
     return render_template('triangle.html', result=result)
 
+@app.route("/works/stack_algo", methods=["GET", "POST"])
+def stack_algo():
+    result = None
+    if request.method == "POST":
+        infix = request.form.get("infix")
+        if infix:
+            result = infix_to_postfix(infix)
+    return render_template("stack_algo.html", result=result)
+
+def infix_to_postfix(expression):
+    precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+    stack = []
+    output = []
+
+    for char in expression:
+        if char.isalnum():
+            output.append(char)
+        elif char == '(':
+            stack.append(char)
+        elif char == ')':
+            while stack and stack[-1] != '(':
+                output.append(stack.pop())
+            stack.pop()
+        else:
+            while stack and stack[-1] != '(' and precedence.get(char, 0) <= precedence.get(stack[-1], 0):
+                output.append(stack.pop())
+            stack.append(char)
+
+    while stack:
+        output.append(stack.pop())
+
+    return ''.join(output)
+
 @app.route('/home')
 def home():
     return render_template('homepage.html')
